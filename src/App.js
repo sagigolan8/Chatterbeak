819e9b3030edff3d3d8c83e1425c6e773d54c418
sagigolan8 <89462403+sagigolan8@users.noreply.github.com>
2022-03-03 00:26:17 +0200

    initialized project with all needed packages and assets

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import GlobalStyle from './globalStyles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+
+//Pages
+import Home from './pages/Home';
+import SignUp from './components/SignUp/SignUp';
+import Pricing from './components/Pricing/Pricing';
+import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy';
+import Footer from './components/Footer/Footer';
+import Terms from './components/Terms/Terms';
+import Disclaimer from './components/Disclaimer/Disclaimer';
+import Report from './components/Report/Report';
+import SignIn from './components/SignIn/SignIn';
+import Safety from './components/Safety/Safety';
+import Feedback from './components/Feedback/Feedback';
+import AllServices from './components/AllServices/AllServices';
+import Profile from './components/Profile/Profile';
+
+function App() {
+	return (
+		<Router>
+			<GlobalStyle />
+			<Navbar />
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/signup" exact component={SignUp} />
+				<Route path="/signin" exact component={SignIn} />
+				<Route path="/pricing" exact component={Pricing} />
+				<Route path="/policy" exact component={PrivacyPolicy} />
+				<Route path="/terms" exact component={Terms} />
+				<Route path="/disclaimer" exact component={Disclaimer} />
+				<Route path="/report" exact component={Report} />
+				<Route path="/safety" exact component={Safety} />
+				<Route path="/feedback" exact component={Feedback} />
+				<Route path="/profile" exact component={Profile} />
+			</Switch>
+			<Footer />
+		</Router>
+	);
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
+//  change port =>  "start": "set PORT=3005 && react-scripts start",
+// trust-safety => https://explore.zoom.us/en/trust/trust-safety/
